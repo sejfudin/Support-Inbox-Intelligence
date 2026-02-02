ce7a1b839d03d32cee9790038cd7b48b16a8cf5f
lenasaraba <lenasaraba8@gmail.com>
2026-02-02 14:34:04 +0100

    refactor: update authService and authentication controller for user registration

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
-exports.registerUser = async (userData) => {
+const register = async (userData) => {
   const { fullName, email, password, role } = userData;
 
   const userExists = await User.findOne({ email });
@@ -19,12 +19,19 @@ exports.registerUser = async (userData) => {
     role: role || 'agent',
     active: true
   });
+
   return {
-    user: {
-      id: user._id,
-      fullname: user.fullname,
-      email: user.email,
-      role: user.role
-    }
+    id: user._id,
+    fullname: user.fullname,
+    email: user.email,
+    role: user.role
   };
+};
+
+const login = async (credentials) => {
+};
+
+module.exports = {
+  register,
+  login
 };
