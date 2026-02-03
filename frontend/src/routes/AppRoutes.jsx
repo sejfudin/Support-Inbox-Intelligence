aa594d94643c35af623efac173eb8dbbd6d0981d
TarikKarahodzic <tariktare@live.com>
2026-02-03 09:21:11 +0100

    Made the SidebarLayout; easier logic for wrapping the desired pages with sidebar

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,8 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { LoginPage } from "@/pages/LoginPage";
 import Register from "@/pages/Register";
 import TicketPage from "@/pages/TicketPage";
-import AppSidebar from "@/components/AppSidebar";
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import SidebarLayout from "@/layouts/SidebarLayout";
 
 export default function AppRoutes() {
   return (
@@ -12,17 +11,10 @@ export default function AppRoutes() {
       <Route path="/" />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<Register />} />
-      <Route
-        path="/tickets"
-        element={
-          <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-              <TicketPage />
-            </SidebarInset>
-          </SidebarProvider>
-        }
-      />
+
+      <Route element={<SidebarLayout />}>
+        <Route path="/tickets" element={<TicketPage />} />
+      </Route>
     </Routes>
   );
 }
