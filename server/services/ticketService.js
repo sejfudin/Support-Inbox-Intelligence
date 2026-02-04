49eb50f3709ad290131d5b1507dc4dd9c8005c05
Danijela Ikanović <danijela.ikanovic@symphony.is>
2026-02-04 09:47:17 +0100

    .

--- server/services/ticketService.js
+++ server/services/ticketService.js
@@ -1,47 +1,51 @@
 const Ticket = require("../models/Ticket");
 
-const getAllTickets= async ({ page = 1, limit = 10, search = "", status = "" })=>{
-    const skip = (page - 1) * limit;
+const getAllTickets = async ({
+  page = 1,
+  limit = 10,
+  search = "",
+  status = "",
+}) => {
+  const skip = (page - 1) * limit;
 
-    const query = {};
-    if (search) {
-        query.$or = [
-            { subject: { $regex: search, $options: 'i' } },
-            { description: { $regex: search, $options: 'i' } },
-        ];
-    }
+  const query = {};
+  if (search) {
+    query.$or = [
+      { subject: { $regex: search, $options: "i" } },
+      { description: { $regex: search, $options: "i" } },
+    ];
+  }
 
-    if (status && status !== "all") {
+  if (status && status !== "all") {
     query.status = status;
   }
 
-    const [tickets, total] = await Promise.all([
-        Ticket.find(query)
-            .sort({ updatedAt: -1 })
-            .skip(skip)
-            .limit(limit)
-            .populate("creator", "fullname email")
-            .populate("assignedTo", "fullname email role"),
+  const [tickets, total] = await Promise.all([
+    Ticket.find(query)
+      .sort({ updatedAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("creator", "fullname email")
+      .populate("assignedTo", "fullname email role"),
     Ticket.countDocuments(query),
-    ]);
-return {
+  ]);
+  return {
     tickets,
-   pagination: {
-        total,
-        page: Number(page),
-        limit: Number(limit), 
-        pages: Math.ceil(total / limit),
+    pagination: {
+      total,
+      page: Number(page),
+      limit: Number(limit),
+      pages: Math.ceil(total / limit),
     },
   };
 };
-
 
 const createTicket = async (ticketData) => {
   const ticket = new Ticket({
     subject: ticketData.subject,
     description: ticketData.description || "",
-    creator: ticketData.creatorId, 
-    status: "pending", 
+    creator: ticketData.creatorId,
+    status: ticketData.status,
     assignedTo: ticketData.assignedTo,
   });
 
@@ -49,7 +53,7 @@ const createTicket = async (ticketData) => {
 
   return await ticket.populate([
     { path: "creator", select: "fullName email" },
-    { path: "assignedTo", select: "fullName email" }
+    { path: "assignedTo", select: "fullName email" },
   ]);
 };
 
