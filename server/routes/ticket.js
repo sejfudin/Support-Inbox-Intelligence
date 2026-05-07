97f69506e5fdaf4860375bd887bc4746b72b7e35
susicharis7 <susicharis99@gmail.com>
2026-05-07 11:12:08 +0200

    feat(routes): add ticket description image routes

--- server/routes/ticket.js
+++ server/routes/ticket.js
@@ -16,12 +16,27 @@ const {
 // const { generateAI } = require('../controllers/ai');
 const { protect } = require('../middleware/auth');
 
+const { uploadImages } = require('../middleware/upload');
+const {
+  getTicketDescriptionImages,
+  uploadTicketDescriptionImages,
+  deleteTicketDescriptionImage,
+  deleteAllTicketDescriptionImages,
+} = require('../controllers/attachmentImage');
+
+
 router.get('/my-tickets', protect, getMyTickets);
 router.get('/', protect, getAllTickets);
 
 router.post('/', protect, createTicket);
 router.post('/suggest-metadata', protect, suggestTicketMetadata);
 router.post('/generate-description', protect, generateTicketDescription);
+
+router.get('/:ticketId/description/images', protect, getTicketDescriptionImages);
+router.post('/:ticketId/description/images', protect, uploadImages, uploadTicketDescriptionImages);
+router.delete('/:ticketId/description/images/:imageId', protect, deleteTicketDescriptionImage);
+router.delete('/:ticketId/description/images', protect, deleteAllTicketDescriptionImages);
+
 router.get('/:id', protect, getTicketById);
 router.patch('/:id', protect, updateTicket);
 router.patch('/:id/archive', protect, archiveTicket);
